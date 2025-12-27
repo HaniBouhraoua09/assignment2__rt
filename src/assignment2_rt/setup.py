c1fec73d549d5a8f3bbd3774671a6440a23539dd
Hani Bouhraoua <hanibouhraoua09@gmail.com>
2025-12-27 16:36:43 +0100

    Final Polish: Added Launch File and cleaned build errors

--- src/assignment2_rt/setup.py
+++ src/assignment2_rt/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'assignment2_rt'
 
@@ -6,22 +8,29 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # we add this 
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
+    
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='hani',
     maintainer_email='hanibouhraoua09@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
+    
+    
     extras_require={
         'test': [
             'pytest',
         ],
     },
+    
+    
     entry_points={
         'console_scripts': [
         'safety_controller = assignment2_rt.safety_node:main',
